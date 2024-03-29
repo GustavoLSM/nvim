711db04b48c477bee7f490524d63eb81383d9c2d
Gustavo Machado <gluismachado@outlook.com>
2024-03-28 21:12:19 -0300

    Adicionar atalhos de teclado

---
+++ lua/keys.lua
@@ -0,0 +1,5 @@
+vim.g.mapleader = " "
+vim.keymap.set("n", "<c-h>", "<C-w>h<CR>")
+vim.keymap.set("n", "<c-j>", "<C-w>j<CR>")
+vim.keymap.set("n", "<c-k>", "<C-w>k<CR>")
+vim.keymap.set("n", "<c-l>", "<C-w>l<CR>")
