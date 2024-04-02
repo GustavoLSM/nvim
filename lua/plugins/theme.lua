68bbac1f39d10de0520e585fa065575736aada91
Gustavo Machado <gluismachado@outlook.com>
2024-04-02 12:47:19 -0300

    Adicionar tema ao editor

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,6 +1,6 @@
 return {
-	"dasupradyumna/midnight.nvim", 
-	lazy = false, 
+	"dasupradyumna/midnight.nvim",
+	lazy = false,
 	priority = 1000,
 	config = function()
 		vim.cmd.colorscheme "midnight"
