7cf4a4ad8f6456dd15d09fd8edb25a2ac85a67ed
Gustavo Machado <gluismachado@outlook.com>
2024-04-11 19:36:38 -0300

    Ajustar arquivo de inicialização

--- init.lua
+++ init.lua
@@ -1,3 +1,4 @@
-require('gustavo.core.global_settings')
-require('gustavo.core.keyboard_shortcuts')
-require('gustavo.core.plugin_manager')
+-- Requires
+require('gustavo.core.global_settings') -- Load global settings
+require('gustavo.core.keyboard_shortcuts') -- Load keyboard shortcuts
+require('gustavo.core.plugin_manager') -- Load plugin manager
