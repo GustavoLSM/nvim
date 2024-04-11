9ed55d8a1f21ca580366a29eb01f95052c3c8b1f
Gustavo Machado <gluismachado@outlook.com>
2024-04-11 20:32:47 -0300

    Adicionar syntax highlighting em linguagens configuradas

---
+++ lua/gustavo/plugins/syntax_highlight.lua
@@ -0,0 +1,16 @@
+-- Treesitter syntax highlighter
+return {
+    'nvim-treesitter/nvim-treesitter', -- Clone syntax highlighting plugin
+    build = ':TSUpdate', -- Treesitter version
+    config = function()
+        -- Local variables
+        local config = require("nvim-treesitter.configs") -- Load Treesitter settings
+
+        -- Syntax highlighting settings
+        config.setup({
+            ensure_installed = { "php", "javascript", "lua", "css", "html" }, -- Automatically configure highlight on these languages
+            highlight = { enable = true }, -- Enable highlighting
+            indent = { enable = true }, -- Enable autoindent on configured languages
+        })
+    end,
+}
