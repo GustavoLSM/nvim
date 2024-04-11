21f94a0ef2a5b779d993ef540eb7daa1bdfde147
Gustavo Machado <gluismachado@outlook.com>
2024-04-11 19:37:32 -0300

    Adicionar tema ao vim

---
+++ lua/gustavo/plugins/theme.lua
@@ -0,0 +1,7 @@
+-- Colorscheme
+return {
+    'CantoroMC/nvim-nightmare', -- Clone the colorscheme
+    config = function()
+        vim.cmd('colorscheme nightmare') -- Load the colorscheme
+    end,
+}
