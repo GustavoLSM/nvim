484e83d3327068c8241aec44aa09cd6dc6e50240
Gustavo Machado <gluismachado@outlook.com>
2024-04-11 20:32:29 -0300

    Adicionar plugin que maximiza os splits

---
+++ lua/gustavo/plugins/split_maximizer.lua
@@ -0,0 +1,7 @@
+-- Vim maximizer
+return {
+    'szw/vim-maximizer', -- Clone the split window maximizer
+    keys = {
+        { '<leader>sm', '<cmd>MaximizerToggle<CR>' }, -- Load split window maximizer
+    },
+}
