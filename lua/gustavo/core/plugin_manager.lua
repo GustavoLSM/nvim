92c458d27324755c507cb1bdd3bb3e98dda5a96e
Gustavo Machado <gluismachado@outlook.com>
2024-04-11 19:12:39 -0300

    Adicionar arquivo de configuração do gerenciador de plugins

---
+++ lua/gustavo/core/plugin_manager.lua
@@ -0,0 +1,17 @@
+-- Local variables
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim' -- Set lazy vim plugin manager path
+
+-- Lazy plugin manager
+if not (vim.uv or vim.loop).fs_stat(lazypath) then -- Clone Lazy vim if not installed on the computer
+  vim.fn.system({
+    'git',
+    'clone',
+    '--filter=blob:none',
+    'https://github.com/folke/lazy.nvim.git',
+    '--branch=stable',
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require('lazy').setup('gustavo.plugins') -- Load Lazy vim
