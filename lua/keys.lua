35991b0af74850118169472d91e5e4cb928f44a9
Gustavo Machado <gluismachado@outlook.com>
2024-04-11 18:12:37 -0300

    Reorganizar repositório

--- lua/keys.lua
+++
@@ -1,5 +0,0 @@
-vim.g.mapleader = " "
-vim.keymap.set("n", "<c-h>", "<C-w>h<CR>")
-vim.keymap.set("n", "<c-j>", "<C-w>j<CR>")
-vim.keymap.set("n", "<c-k>", "<C-w>k<CR>")
-vim.keymap.set("n", "<c-l>", "<C-w>l<CR>")
--- lua/sets.lua
+++
@@ -1,7 +0,0 @@
-vim.cmd("set nu")
-vim.cmd("set rnu")
-vim.cmd("set nowrap")
-vim.cmd("set tabstop=4")
-vim.cmd("set softtabstop=4")
-vim.cmd("set shiftwidth=4")
-vim.cmd("set noexpandtab")
