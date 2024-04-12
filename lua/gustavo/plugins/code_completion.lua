e6a3cb0e22be6f8630de904deff0f2fa937a1413
Gustavo Machado <gluismachado@outlook.com>
2024-04-11 23:41:05 -0300

    Ajustar auto completar de código

--- lua/gustavo/plugins/code_completion.lua
+++ lua/gustavo/plugins/code_completion.lua
@@ -1,50 +1,61 @@
 -- Nvim CMP
 return {
-    {
-        'hrsh7th/cmp-nvim-lsp' -- Clone sources for the completion engine
-    },
-    {
-        'L3MON4D3/LuaSnip', -- Clone the snippet engine
-        dependencies = {
-            'saadparwaiz1/cmp_luasnip', -- Enable code completion
-            'rafamadriz/friendly-snippets', -- Useful snippets
+    'hrsh7th/nvim-cmp', -- Code completion engine
+    event = 'InsertEnter', -- Enable only in insert mode
+    dependencies = {
+        'hrsh7th/cmp-buffer', -- Source for text in current file
+        'hrsh7th/cmp-path', -- Source for system paths
+        {
+            'L3MON4D3/LuaSnip', -- Snippet engine
+            version = 'v2.*', -- LuaSnip version 
         },
+        'saadparwaiz1/cmp_luasnip', -- Snippets for code completion
+        'rafamadriz/friendly-snippets', -- Useful snippets
+        'onsails/lspkind.nvim', -- Vs-code like pictograms
     },
-    {
-        'hrsh7th/nvim-cmp', -- Clone the completion engine
-        config = function()
-            -- Local variables
-            local cmp = require('cmp') -- Enable code completion
+    config = function()
+        -- Local variables
+        local cmp = require('cmp') -- Load code completion engine
+        local luasnip = require('luasnip') -- Load snippet engine
+        local lspkind = require('lspkind') -- Load pictogram engine
 
-            -- Code completion settings
-            cmp.setup({
-                -- Snippet engine settings
-                snippet = {
-                    expand = function(args)
-                        require('luasnip').lsp_expand(args.body) -- Load snippet engine
-                    end,
-                },
+        -- Vs-code style snippets
+        require('luasnip.loaders.from_vscode').lazy_load() -- Enable Vs-code style snippets 
 
-                -- Code completion UI
-                window = {
-                    completion = cmp.config.window.bordered(), -- Enable UI for code completion
-                    documentation = cmp.config.window.bordered(), -- Enable UI for code documentation
-                },
+        cmp.setup({
+            -- Code completion engine settings
+            completion = {
+                completeopt = 'menu,menuone,preview,noselect', -- Enable code completion
+            },
+            -- Snippet engine settings
+            snippet = {
+                expand = function(args)
+                    luasnip.lsp_expand(args.body) -- Enable interaction between completion engine and snippet engine
+                end,
+            },
+            -- Keyboard shortcuts
+            mapping = cmp.mapping.preset.insert({
+                ['<C-j>'] = cmp.mapping.select_next_item(), -- Next suggestion
+                ['<C-k>'] = cmp.mapping.select_prev_item(), -- Previous suggestion
+                ['<C-Space>'] = cmp.mapping.complete(), -- Show completion suggestions
+                ['<C-e>'] = cmp.mapping.abort(), -- Close completion window
+                ['<CR>'] = cmp.mapping.confirm({ select = false }), -- Confirm completion
+            }),
 
-                -- Keyboard shortcuts
-                mapping = cmp.mapping.preset.insert({
-                    ['<C-Space>'] = cmp.mapping.complete(), -- Open up the completion UI
-                    ['<C-e>'] = cmp.mapping.abort(), -- Cancel the completion
-                    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Confirm the completion
+            -- Sources for code completion
+            sources = cmp.config.sources({
+                { name = 'luasnip' }, -- Snippet engine
+                { name = 'buffer' }, -- Text in the current file
+                { name = 'path' }, -- System paths
+            }),
+
+            -- Vs-code like pictograms in completion menu
+            formatting = {
+                format = lspkind.cmp_format({
+                    maxwidth = 50,
+                    ellipsis_char = "...",
                 }),
-
-                -- Sources
-                sources = cmp.config.sources({
-                    { name = 'nvim_lsp' }, -- Completion from LSP
-                    { name = 'luasnip' }, -- Completion from snippet engine
-                    { name = 'buffer' }, -- Completion from current file
-                }),
-            })
-        end,
-    },
+            },
+        })
+    end,
 }
