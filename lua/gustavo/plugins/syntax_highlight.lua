ac0a576fcd82cc69e2d3b5d9d74e09fe324d5558
Gustavo Machado <gluismachado@outlook.com>
2024-04-12 00:04:42 -0300

    Ajustar syntax highlight

--- lua/gustavo/plugins/syntax_highlight.lua
+++ lua/gustavo/plugins/syntax_highlight.lua
@@ -4,11 +4,11 @@ return {
     build = ':TSUpdate', -- Treesitter version
     config = function()
         -- Local variables
-        local config = require("nvim-treesitter.configs") -- Load Treesitter settings
+        local config = require('nvim-treesitter.configs') -- Load Treesitter settings
 
         -- Syntax highlighting settings
         config.setup({
-            ensure_installed = { "php", "javascript", "lua", "css", "html" }, -- Automatically configure highlight on these languages
+            ensure_installed = { 'php', 'javascript', 'lua', 'css', 'html' }, -- Automatically configure highlight on these languages
             highlight = { enable = true }, -- Enable highlighting
             indent = { enable = true }, -- Enable autoindent on configured languages
         })
